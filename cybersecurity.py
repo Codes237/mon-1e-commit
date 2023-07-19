aa461b4cfa45854fe2ae88e409faf8ab7da5fab5
Wilson <139231892+Codes237@users.noreply.github.com>
2023-07-18 21:29:15 -0400

    Add files via upload

---
+++ cybersecurity.py
@@ -0,0 +1,25 @@
+# Cybersecurity By Wilson CHOUNDONG =========================================
+"""
+Python
+                Training
+"""
+
+question1 = input("What can I learn in this Cybersecurity Boot Camp? ")
+
+print(" Ho! Thanks to share this with me. So I should learn :" + question1 + "?")
+
+print()
+
+question2 = input("When can I start it ? ")
+print(question2)
+
+print()
+response3 = input("Do you have any recommendations  ")
+print(response3)
+
+print("Great.. Thank you!")
+
+print()
+
+y_w = input("")
+print(y_w)
